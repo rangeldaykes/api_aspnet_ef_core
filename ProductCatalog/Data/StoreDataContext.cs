6711e21cbb304ec5ba6d1e53cf760a1fbfb1abd8
rangel <daykes@gmail.com>
2019-07-12 15:31:35 -0300

    Introdução ao Entity Framework Core

---
+++ ProductCatalog/Data/StoreDataContext.cs
@@ -0,0 +1,23 @@
+
+using Microsoft.EntityFrameworkCore;
+using ProductCatalog.Models;
+
+namespace ProductCatalog.Data
+{
+    public class StoreDataContext : DbContext
+    {
+        public DbSet<Product> Produtos { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=localhost,1433;Database=pdtctl;User ID=SA; Password=R$NG%Lr4ng5l");
+        }
+
+/*         protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfiguration(new ProductMap());
+            builder.ApplyConfiguration(new CategoryMap());
+        } */
+    }    
+}
