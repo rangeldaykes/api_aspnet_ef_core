b0620e9824bf387cb562ab03beac8d6aebb9aa80
rangel daykes <daykes@gmail.com>
2019-07-14 21:53:54 -0300

    Rotas e Crud

--- ProductCatalog/Data/StoreDataContext.cs
+++ ProductCatalog/Data/StoreDataContext.cs
@@ -7,7 +7,7 @@ namespace ProductCatalog.Data
 {
     public class StoreDataContext : DbContext
     {
-        public DbSet<Product> Produtos { get; set; }
+        public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
